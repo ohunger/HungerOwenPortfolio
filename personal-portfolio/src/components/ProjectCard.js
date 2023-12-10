7d565510f02bf4041a118102b911830bb639124e
ohunger <owenhunger@hotmail.com>
2023-12-09 18:59:52 -0800

    updated projects, still no email

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -1,3 +1,4 @@
+/*
 import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
@@ -13,3 +14,23 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
     </Col>
   )
 }
+*/
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+export const ProjectCard = ({ title, description, imgUrl, linkTo }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+      <a href={linkTo} target="_blank" rel="noopener noreferrer" >
+        <img src={imgUrl} alt={title} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+        
+      </a>
+      </div>
+    </Col>
+  );
+};
--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -4,24 +4,29 @@ import { ProjectCard } from "./ProjectCard";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import projImg1 from '../assets/img/eebo.png';
+import projImg2 from '../assets/img/signlang.png';
+import projImg3 from '../assets/img/blendify.png';
 
 export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      //imgUrl: projImg1,
+      title: "EEBO",
+      description: "Online Marketplace with Google maps integration. HTML/CSS/JS/Firebase/API",
+      imgUrl: projImg1,
+      linkTo: "https://eebo-c2acf.web.app/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      //imgUrl: projImg2,
+      title: "Blendify",
+      description: "Song recommendations Webapp for DJ's build on spotify API.",
+      imgUrl: projImg3,
+      linkTo: "https://github.com/connorsavage/Blendify/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      //imgUrl: projImg3,
+      title: "Live Sign Language Recognition",
+      description: "Computer vision ML project using python/scikit-learn/tensorflow/opencv/mediapipe",
+      imgUrl: projImg2,
     },
   ];
 
@@ -33,7 +38,7 @@ export const Projects = () => {
             
               <div className="">
                 <h2>Projects</h2>
-                <p>Short collection of some of my projects</p>
+                <p>Short collection of some of my projects. Click on any project to see it in action.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Tab.Content id="slideInUp" className="">
                     <Tab.Pane eventKey="first">
